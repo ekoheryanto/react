c741d767e2df9ea3bea7a45b842447bb2f0cd1ab
ekoheryanto <eko.heryanto94@gmail.com>
2018-11-05 21:51:05 +0700

    edit link

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ class App extends Component {
           <p>
           @firmware_studio
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Belajar React
           </a>
         </header>
