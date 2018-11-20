c16f762efb0f25f356c2b5bdfe262475d0056a13
ekoheryanto <eko.heryanto94@gmail.com>
2018-11-20 20:52:02 +0700

    cara koneksi antar file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          @firmware_studio
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Belajar React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <h3>Rendering Elements</h3>
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+  render(){
+    return(
+      <h1>Ini Latihan React js</h1>
+    );
+  }
+}
+
+export default Header;
