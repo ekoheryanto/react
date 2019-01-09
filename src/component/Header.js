51449f7a984d0f426fb9d38bf233830f57e90720
ekoheryanto <eko.heryanto94@gmail.com>
2019-01-09 20:04:52 +0700

    componentDidMount dan kondisi rendering

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,9 @@ class Header extends Component{
   super(props);
   this.state = {
     data : "Daftar Makanan Nusantara",
-    datalist : this.props.list
+    datalist : this.props.list,
+    statusRendering : true
+
   };
   this.handleklik = this.handleklik.bind(this);
 }
@@ -16,15 +18,28 @@ alert(this.state.data);
 alert(value);
 };
 
+/*componentDidMount akan jalan secara otomatis setelah rendering*/
+componentDidMount (){
+console.log("jalan : componentDidMount")
+
+}
+
   render(){
+    console.log("jalan : Render")
+    /*penulisan kondisi rendering inline*/
     return(
       <div>
-      <h1>Makanan Nusantara</h1>
-      <p>{this.state.data}</p>
-      <p>{this.state.datalist}</p>
-      <a href="/" onClick={e=>this.handleklik("Pesan Dari Header",e)}>Header</a>
+       {this.state.statusRendering === true ? (<h1>Selamat Datang</h1>):(<h1>Selamat Tinggal</h1>)}
       </div>
     );
+    // return(
+    //   <div>
+    //   <h1>Makanan Nusantara</h1>
+    //   <p>{this.state.data}</p>
+    //   <p>{this.state.datalist}</p>
+    //   <a href="/" onClick={e=>this.handleklik("Pesan Dari Header",e)}>Header</a>
+    //   </div>
+    // );
   }
 }
 
