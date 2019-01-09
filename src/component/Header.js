cde88ceb88ba2c29b43b2857bf170262a2dec0f5
ekoheryanto <eko.heryanto94@gmail.com>
2019-01-09 20:50:02 +0700

    conditional rendering 2,3

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,16 +6,23 @@ class Header extends Component{
   this.state = {
     data : "Daftar Makanan Nusantara",
     datalist : this.props.list,
-    statusRendering : true
+    statusRendering : false
 
   };
   this.handleklik = this.handleklik.bind(this);
+  this.handleElement = this.handleElement.bind(this);
 }
 
 handleklik(value, e){
 e.preventDefault();
 alert(this.state.data);
 alert(value);
+};
+
+handleElement(){
+  this.setState((state, props)=>{
+    return{statusRendering : !state.statusRendering }
+  });
 };
 
 /*componentDidMount akan jalan secara otomatis setelah rendering*/
@@ -26,10 +33,22 @@ console.log("jalan : componentDidMount")
 
   render(){
     console.log("jalan : Render")
-    /*penulisan kondisi rendering inline*/
+    /*penulisan kondisi rendering inline
+    1.Apabila dalam kondisi rendering lebih dari satu element maka harus memakai pembungkus <div>*/
     return(
       <div>
-       {this.state.statusRendering === true ? (<h1>Selamat Datang</h1>):(<h1>Selamat Tinggal</h1>)}
+       {this.state.statusRendering === true ? (
+         <div>
+         <h1>Selamat Datang</h1>
+         <h2>Silahakan Pilih Menu</h2>
+         </div>
+       ):(
+         <div>
+         <h1>Terima Kasih Atas Kunjungan Anda</h1>
+         <h2>Silahkan Datang Kembali</h2>
+         </div>
+       )}
+       <button onClick={this.handleElement}>Rubah</button>
       </div>
     );
     // return(
