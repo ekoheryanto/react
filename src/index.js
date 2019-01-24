d270d7fada5b8ccb72058bc73d865ee2a011082e
ekoheryanto <eko.heryanto94@gmail.com>
2019-01-24 19:42:29 +0700

    react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './component/App';
+import App from './component/home/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
